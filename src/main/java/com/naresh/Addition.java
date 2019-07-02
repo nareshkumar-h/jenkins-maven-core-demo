11b0eb46173c75ab3895402e286ee3c161428dca
Naresh Kumar H <nareshkumarh@live.com>
2019-07-01 22:12:00 -0400

    Added Jenkinsfile

---
+++ src/main/java/com/naresh/Addition.java
@@ -0,0 +1,9 @@
+package com.naresh;
+
+public class Addition {
+
+	public static int add(int a,int b) {
+		if ( a <0 || b< 0) return 0;
+		return a+b;
+	}
+}
---
+++ src/test/java/com/naresh/TestAddition.java
@@ -0,0 +1,15 @@
+package com.naresh;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestAddition {
+
+	@Test
+	public void test() {
+		int result = Addition.add(2, 3);
+		assertEquals(5, result);
+	}
+
+}
